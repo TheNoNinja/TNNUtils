8ce1f087b8c6f6bc9b2381cccad15d9288bdea4d
TheNoNinja <Christiaanbrant@gmail.com>
2021-07-20 20:34:52 +0200

    Added perlin

---
+++ Random/Examples/PerlinNoiseOnTexture.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace TNNUtils.Random.Examples
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    [RequireComponent(typeof(MeshFilter))]
+    public class PerlinNoiseOnTexture : MonoBehaviour
+    {
+        public int width = 256;
+        public int height = 256;
+        public float scale = 20;
+
+        private MeshRenderer _meshRenderer;
+        
+        public void Awake()
+        {
+            //Just some code to generate a quad on runtime to display the perlin noise. You can ignore this if you already have a quad or want to use the perlin noise for something else
+            var meshFilter = gameObject.GetComponent<MeshFilter>();
+            meshFilter.mesh = new Mesh
+            {
+                vertices = new Vector3[]
+                {
+                    new (0, 0, 0),
+                    new (1, 0, 0),
+                    new (0, 1, 0),
+                    new (1, 1, 0)
+                },
+                triangles = new []
+                {
+                    0, 2, 1,
+                    2, 3, 1
+                },
+                normals = new []
+                {
+                    -Vector3.forward,
+                    -Vector3.forward,
+                    -Vector3.forward,
+                    -Vector3.forward
+                },
+                uv = new Vector2[]
+                {
+                    new (0, 0),
+                    new (1, 0),
+                    new (0, 1),
+                    new (1, 1)
+                }
+            };
+            _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            
+        }
+
+        private void LateUpdate()
+        {
+            //Generate a new texture every physics update
+            _meshRenderer.material.mainTexture = GenerateTexture();
+        }
+
+        private Texture2D GenerateTexture()
+        {
+            var texture = new Texture2D(width, height);
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    texture.SetPixel(x, y, GenerateColor(x, y));
+                } 
+            }
+            
+            texture.Apply();
+            return texture;
+        }
+
+        private Color GenerateColor(float x, float y)
+        {
+            var xCoordinate = x / width * scale;
+            var yCoordinate = y / height * scale;
+            return new Color(Perlin.PerlinNoise(xCoordinate, yCoordinate), 
+                             Perlin.PerlinNoise(xCoordinate, yCoordinate), 
+                             Perlin.PerlinNoise(xCoordinate, yCoordinate));
+        }
+    }
+}
---
+++ Random/Perlin.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace TNNUtils.Random
+{
+    public static class Perlin
+    {
+        private static float Interpolate(float from, float to, float weight)
+        {
+            if (0f > weight) return from;
+            if (1f < weight) return to;
+            
+            return (to - from) * ((weight * (weight * 6f - 15f) + 10f) * weight * weight * weight) + from;
+        }
+
+        private static Vector2 RandomGradient(int x, int y)
+        {
+            var random = 2920f * Mathf.Sin(x * 21942f + y * 171324f + 8912f) * Mathf.Cos(x * 23157 * y * 217832 + 9758);
+
+            return new Vector2(Mathf.Cos(random), Mathf.Sin(random));
+        }
+
+        private static float DotGridGradient(int x, int y, float xOffset, float yOffset)
+        {
+            var gradient = RandomGradient(x, y);
+
+            var xDistance = xOffset - x;
+            var yDistance = yOffset - y;
+
+            return xDistance * gradient.x + yDistance * gradient.y;
+        }
+
+        public static float PerlinNoise(float x, float y)
+        {
+            var x0 = (int)x;
+            var x1 = x0 + 1;
+            var y0 = (int)y;
+            var y1 = y0 + 1;
+
+            var xWeight = x - x0;
+            var yWeight = y - y0;
+            
+            var xValue00 = DotGridGradient(x0, y0, x, y);
+            var xValue01 = DotGridGradient(x1, y0, x, y);
+            var xValue0 = Interpolate(xValue00, xValue01, xWeight);
+
+            var xValue10 = DotGridGradient(x0, y1, x, y);
+            var xValue11 = DotGridGradient(x1, y1, x, y);
+            var xValue1 = Interpolate(xValue10, xValue11, xWeight);
+
+            return (Interpolate(xValue0, xValue1, yWeight) + 1) / 2;
+        }
+    }
+}
